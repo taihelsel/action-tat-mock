d985b6fd080eab1c6c75f69ce78c8223c9d2aece
taihelsel <tai.bayard@gmail.com>
2018-04-02 00:24:29 -0700

    added landing page links

--- src/Home/Landing.js
+++ src/Home/Landing.js
@@ -4,6 +4,10 @@ class Landing extends Component {
   render() {
     return (
         <section id="landing">
+            <ul className="landing-links">
+                <li className="landing-link-item"><a>View Shop Gallery</a></li>
+                <li className="landing-link-item"><a>Book Now</a></li>
+            </ul>
             <video loop autoPlay className="landing-video">
                 <source src={require(".././videos/tattoo-closeup.mp4")} type="video/mp4" />
             </video>
